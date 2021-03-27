fb0c1e0b6e2dc71f27bb9eda7e86896b1d975dba
unknown <akhilice13@gmail.com>
2021-03-27 19:49:13 +0530

    Added the new category

--- frontend/src/Screens/HomeScreens.js
+++ frontend/src/Screens/HomeScreens.js
@@ -190,7 +190,85 @@ const HomeScreens = () => {
                   ))}
                 </ScrollContainer>
               </div>
+
+
+
+              <div className="Products_Scroll3">
+                <p style={{ fontSize: '14px', marginBottom: '20px', letterSpacing:"1px" }}>
+                  Best Seller's
+                </p>
+                <ScrollContainer className="Scroll_container">
+                  {products.map((product) => (
+                    <div className="Card2">
+                      <div className="Card_Img2">
+                        <Link
+                          className="Link_Card"
+                          to={`/product/${product._id}`}
+                        >
+                          <img
+                            src={product.image}
+                            width="120px"
+                            height="100px"
+                          ></img>
+                        </Link>
+                      </div>
+                      <div className="Product_Details">
+                        <Link
+                          className="Link_Card"
+                          to={`/product/${product._id}`}
+                        >
+                          <p className="Product_Title2">
+                            {product.name}
+                            <span className="Rating">
+                              {product.rating}
+                              <i
+                                style={{
+                                  color: 'yellow',
+                                  marginLeft: '4px',
+                                  fontSize: '8px',
+                                }}
+                                className="fas fa-star"
+                              ></i>
+                            </span>
+                          </p>
+                        </Link>
+
+                        <div className="Product_Brief2">
+                          <p>{product.description}</p>
+                          {/* <p>In Stocks- {product.countInStock}</p> */}
+                        </div>
+                        <div className="Price_Rating">
+                          <span className="Price">
+                            <span
+                              style={{
+                                fontSize: '14px',
+                                fontWeight: '200',
+                                marginRight: '3px',
+                              }}
+                            >
+                              $
+                            </span>
+                            {product.price}
+                          </span>
+                        </div>
+
+                        <Link
+                          className="Link_Card"
+                          to={`/product/${product._id}`}
+                        >
+                          <div className="Btn_Add">
+                            <p>Explore</p>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </ScrollContainer>
+              </div>
             </div>
+
+
+            
             <div className="Premium_Products">
               <p style={{ fontSize: '14px', marginBottom: '20px' , letterSpacing:"1px"}}>
                 Top Pick's
